b33949e06e53625006997236db3aac5a3aa98e9e
Suraj Kumar <surbhi.kumari246@gmail.com>
2024-12-12 22:54:56 +0530

    trial changes

--- peerjs-server/server.js
+++ peerjs-server/server.js
@@ -1,4 +1,22 @@
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: 8988, path: '/peerjs' });
+const peerServer = PeerServer(
+    {
+        port: 8988,
+        path: '/peerjs',
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST'],
+        },
+        allow_discovery: true,
 
-console.log('PeerJS server running on http://localhost:8988');
+    }
+);
+peerServer.on('listening', () => {
+    console.log('PeerJS server running on http://localhost:8988');
+});
+
+// Error handling in case the server cannot start
+peerServer.on('error', (err) => {
+    console.error('Error occurred:', err);
+    process.exit(1);  // Exit the process if there is an error
+});
